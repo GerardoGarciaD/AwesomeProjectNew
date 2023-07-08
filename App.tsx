0c2baaf29518a8a45a81a68210ed9da47ad7befa
Gerardo <geraz26@hotmail.com>
2023-07-08 12:42:12 -0600

    Creando estructura inicial para mostrar la informaciòn con scroll horizontal e informaciòn

--- App.tsx
+++ App.tsx
@@ -1,11 +1,62 @@
-import React from 'react';
-import {Pressable, SafeAreaView, ScrollView, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  FlatList,
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+} from 'react-native';
 import Title from './components/Title/Title';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
 import style from './assets/styles/main';
+import UserStory from './components/UserStory/UserStory';
 
 function App() {
+  const data = [
+    {
+      firstName: 'John',
+      id: 1,
+    },
+    {
+      firstName: 'Angel',
+      id: 2,
+    },
+    {
+      firstName: 'White',
+      id: 3,
+    },
+    {
+      firstName: 'Olivier',
+      id: 4,
+    },
+    {
+      firstName: 'Tyler',
+      id: 5,
+    },
+    {
+      firstName: 'Stephan',
+      id: 6,
+    },
+    {
+      firstName: 'Michael',
+      id: 7,
+    },
+    {
+      firstName: 'Tomas',
+      id: 8,
+    },
+    {
+      firstName: 'Federic',
+      id: 9,
+    },
+  ];
+  const pageSize = 4;
+  const [pageNumber, setPageNumber] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [renderedData, setRenderedData] = useState([]);
+
   return (
     <SafeAreaView>
       <ScrollView>
@@ -17,6 +68,16 @@ function App() {
               <Text style={style.messageNumber}>2</Text>
             </View>
           </Pressable>
+        </View>
+
+        <View style={style.storyContainer}>
+          <FlatList
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            data={data}
+            renderItem={({item}) => (
+              <UserStory firstName={item.firstName} />
+            )}></FlatList>
         </View>
       </ScrollView>
     </SafeAreaView>
