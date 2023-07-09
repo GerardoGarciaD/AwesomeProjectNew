4e0c698202adf525ae7cdd1f932971d1a78b7eb7
Gerardo <geraz26@hotmail.com>
2023-07-09 13:23:35 -0600

    Añadiendo comentario

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -9,6 +9,7 @@ const MainNavigation = () => {
   return (
     <Stack.Navigator
       initialRouteName={Routes.Home}
+      // Esto  es para que no se muestre el header (el titulo de la pagina en la parte superior)
       screenOptions={{header: () => null, headerShown: false}}>
       <Stack.Screen name={Routes.Home} component={Home} />
       <Stack.Screen name={Routes.Profile} component={Profile} />
