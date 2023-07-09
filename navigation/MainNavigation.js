a209e00edfd6df92bdc59e470715dfd8e1bb477d
Gerardo <geraz26@hotmail.com>
2023-07-09 14:23:26 -0600

    Instalando dependencias para implementar drawer navigation

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -2,8 +2,19 @@ import {createStackNavigator} from '@react-navigation/stack';
 import Home from '../srceens/Home/Home';
 import Profile from '../srceens/Profile/Profile';
 import Routes from './Routes';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const MainMenuNavigation = () => {
+  return (
+    <Drawer.Navigator screenOptions={{header: () => null}}>
+      <Drawer.Screen name={Routes.Home} component={Home} />
+      <Drawer.Screen name={Routes.Profile} component={Profile} />
+    </Drawer.Navigator>
+  );
+};
 
 const MainNavigation = () => {
   return (
@@ -11,8 +22,7 @@ const MainNavigation = () => {
       initialRouteName={Routes.Home}
       // Esto  es para que no se muestre el header (el titulo de la pagina en la parte superior)
       screenOptions={{header: () => null, headerShown: false}}>
-      <Stack.Screen name={Routes.Home} component={Home} />
-      <Stack.Screen name={Routes.Profile} component={Profile} />
+      <Stack.Screen name="Drawer" component={MainMenuNavigation}></Stack.Screen>
     </Stack.Navigator>
   );
 };
