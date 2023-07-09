4b61b2894cc4c2667f49d79bd7c0ed6fccae22ee
Gerardo <geraz26@hotmail.com>
2023-07-08 20:02:13 -0600

    Refactorizando codigo para evitar error de nested virtualized https://www.udemy.com/course/react-native-mobile-app-development/learn/lecture/36397932

--- App.tsx
+++ App.tsx
@@ -1,18 +1,12 @@
 /* eslint-disable react/self-closing-comp */
 import React, {useState} from 'react';
-import {
-  FlatList,
-  Pressable,
-  SafeAreaView,
-  ScrollView,
-  Text,
-  View,
-} from 'react-native';
+import {FlatList, Pressable, SafeAreaView, Text, View} from 'react-native';
 import Title from './components/Title/Title';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
 import style from './assets/styles/main';
 import UserStory from './components/UserStory/UserStory';
+import UserPost from './components/UserPost/UserPost';
 
 function App() {
   const data = [
@@ -53,56 +47,152 @@ function App() {
       id: 9,
     },
   ];
+
+  const posts = [
+    {
+      firstName: 'Allison',
+      lastName: 'Becker',
+      location: 'Sukabumi, Jawa Barat',
+      likes: 1201,
+      comments: 24,
+      bookmarks: 55,
+      id: 1,
+    },
+    {
+      firstName: 'Jennifer',
+      lastName: 'Wilkson',
+      location: 'Pondok Leungsir, Jawa Barat',
+      likes: 570,
+      comments: 12,
+      bookmarks: 60,
+      id: 2,
+    },
+    {
+      firstName: 'Adam',
+      lastName: 'Spera',
+      location: 'Boston, Massachusetts',
+      likes: 100,
+      comments: 8,
+      bookmarks: 7,
+      id: 3,
+    },
+    {
+      firstName: 'Nata',
+      lastName: 'Vacheishvili',
+      location: 'New York, New York',
+      likes: 300,
+      comments: 18,
+      bookmarks: 17,
+      id: 4,
+    },
+    {
+      firstName: 'Nicolas',
+      lastName: 'Namoradze',
+      location: 'Berlin, Germany',
+      likes: 1240,
+      comments: 56,
+      bookmarks: 20,
+      id: 5,
+    },
+  ];
+
   const pageSize = 4;
   const [pageNumber, setPageNumber] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
   const [renderedData, setRenderedData] = useState(data.slice(0, pageSize));
 
+  const pageSizePosts = 2;
+  const [pageNumberPosts, setPageNumberPosts] = useState(1);
+  const [renderedDataPosts, setRenderedDataPosts] = useState(
+    posts.slice(0, pageSizePosts),
+  );
+  const [isLoadingPosts, setIsLoadingPosts] = useState(false);
+
   // eslint-disable-next-line @typescript-eslint/no-shadow
-  const pagination = (data, pageNumber, pageSize) => {
+  const pagination = (data, pageNumber, pageSize, posts = false) => {
     const startIndex = (pageNumber - 1) * pageSize;
     if (startIndex >= data.length) {
       return [];
     }
-    setPageNumber(pageNumber);
+    if (!posts) {
+      setPageNumber(pageNumber);
+    } else {
+      setPageNumberPosts(pageNumber);
+    }
     return data.slice(startIndex, startIndex + pageSize);
   };
 
   return (
     <SafeAreaView>
-      <ScrollView>
-        <View style={style.header}>
-          <Title title="Lets explore"></Title>
-          <Pressable style={style.messageIcon}>
-            <FontAwesomeIcon size={20} color="#CACDDE" icon={faEnvelope} />
-            <View style={style.messageNumberContainer}>
-              <Text style={style.messageNumber}>2</Text>
+      <FlatList
+        ListHeaderComponent={
+          <>
+            <View style={style.header}>
+              <Title title="Lets explore"></Title>
+              <Pressable style={style.messageIcon}>
+                <FontAwesomeIcon size={20} color="#CACDDE" icon={faEnvelope} />
+                <View style={style.messageNumberContainer}>
+                  <Text style={style.messageNumber}>2</Text>
+                </View>
+              </Pressable>
             </View>
-          </Pressable>
-        </View>
 
-        <View style={style.storyContainer}>
-          <FlatList
-            //Esto se se utiliza para indicar que se ha llegado a la mitad del scroll y se debe cargar mas data
-            onEndReachedThreshold={0.5}
-            //Aqui es donde se carga mas informacion basado en el numero puesto arriba
-            onEndReached={() => {
-              if (!isLoading) {
-                setIsLoading(true);
-                setRenderedData(prev => [
-                  ...prev,
-                  ...pagination(data, pageNumber + 1, pageSize),
-                ]);
-                setIsLoading(false);
-              }
-            }}
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            data={renderedData}
-            renderItem={({item}) => <UserStory firstName={item.firstName} />}
-            keyExtractor={item => item.id.toString()}></FlatList>
-        </View>
-      </ScrollView>
+            <View style={style.storyContainer}>
+              <FlatList
+                //Esto se se utiliza para indicar que se ha llegado a la mitad del scroll y se debe cargar mas data
+                onEndReachedThreshold={0.5}
+                //Aqui es donde se carga mas informacion basado en el numero puesto arriba
+                onEndReached={() => {
+                  if (!isLoading) {
+                    setIsLoading(true);
+                    setRenderedData(prev => [
+                      ...prev,
+                      ...pagination(data, pageNumber + 1, pageSize),
+                    ]);
+                    setIsLoading(false);
+                  }
+                }}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                data={renderedData}
+                renderItem={({item}) => (
+                  <UserStory firstName={item.firstName} />
+                )}
+                keyExtractor={item => item.id.toString()}
+              />
+            </View>
+          </>
+        }
+        //Esto se se utiliza para indicar que se ha llegado a la mitad del scroll y se debe cargar mas data
+        onEndReachedThreshold={0.5}
+        //Aqui es donde se carga mas informacion basado en el numero puesto arriba
+        onEndReached={() => {
+          if (!isLoadingPosts) {
+            setIsLoadingPosts(true);
+            setRenderedDataPosts(prev => [
+              ...prev,
+              ...pagination(posts, pageNumberPosts + 1, pageSizePosts, true),
+            ]);
+            setIsLoadingPosts(false);
+          }
+        }}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+        data={renderedDataPosts}
+        renderItem={({item}) => (
+          <View style={style.userPostContainer}>
+            <UserPost
+              firstName={item.firstName}
+              lastName={item.lastName}
+              comments={item.comments}
+              likes={item.likes}
+              location={item.location}
+              bookmarks={item.bookmarks}
+            />
+          </View>
+        )}
+        keyExtractor={item => item.id.toString()}
+      />
     </SafeAreaView>
   );
 }
