70745f4bc411bc399c768209478d3871075d2b6f
Gerardo <geraz26@hotmail.com>
2023-07-09 21:03:30 -0600

    Añadiendo dependencias para navegación por tabs

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -3,9 +3,43 @@ import Home from '../srceens/Home/Home';
 import Profile from '../srceens/Profile/Profile';
 import Routes from './Routes';
 import {createDrawerNavigator} from '@react-navigation/drawer';
-
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import {Text, View} from 'react-native';
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
+const ProfileTabs = createMaterialTopTabNavigator();
+
+const Tab1 = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Tab 1</Text>
+    </View>
+  );
+};
+const Tab2 = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Tab 2</Text>
+    </View>
+  );
+};
+const Tab3 = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Tab 3</Text>
+    </View>
+  );
+};
+
+export const ProfileTabNavigation = () => {
+  return (
+    <ProfileTabs.Navigator>
+      <ProfileTabs.Screen name="Tab1" component={Tab1} />
+      <ProfileTabs.Screen name="Tab2" component={Tab2} />
+      <ProfileTabs.Screen name="Tab3" component={Tab3} />
+    </ProfileTabs.Navigator>
+  );
+};
 
 const MainMenuNavigation = () => {
   return (
